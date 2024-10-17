093ff3322399bcf859187eb43c3fcfed5ef0154a
MehediMubin <mehedi.cseulab201@gmail.com>
2024-10-17 11:07:55 +0600

    created Task Component

--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
 const TaskBoard = () => {
+   const defaultTask = {
+      id: crypto.randomUUID(),
+      title: "Learn React",
+      description:
+         "React is a UI Library built by Meta. I am going to learn it.",
+      tags: ["Web", "React", "JS"],
+      priority: "High",
+      isFavorite: false,
+   };
+   const [tasks, setTasks] = useState([defaultTask]);
    return (
       <section className="mb-20" id="tasks">
          <div className="container">
             <SearchTask />
             <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
                <TaskActions />
-               <TaskList />
+               <TaskList tasks={tasks} />
             </div>
          </div>
       </section>
