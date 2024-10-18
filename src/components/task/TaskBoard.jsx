04ec94fce8d251ed9e2a3393814b3001727c13c6
MehediMubin <mehedi.cseulab201@gmail.com>
2024-10-18 18:00:30 +0600

    create AddTaskModal component

--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
@@ -14,16 +15,24 @@ const TaskBoard = () => {
       isFavorite: false,
    };
    const [tasks, setTasks] = useState([defaultTask]);
+   const [showModal, setShowModal] = useState(false);
+
+   const handleAddTask = () => {
+      setShowModal(!showModal);
+   };
    return (
-      <section className="mb-20" id="tasks">
-         <div className="container">
-            <SearchTask />
-            <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-               <TaskActions />
-               <TaskList tasks={tasks} />
+      <>
+         {showModal && <AddTaskModal />}
+         <section className="mb-20" id="tasks">
+            <div className="container">
+               <SearchTask />
+               <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+                  <TaskActions onAddTask={handleAddTask} />
+                  <TaskList tasks={tasks} />
+               </div>
             </div>
-         </div>
-      </section>
+         </section>
+      </>
    );
 };
 
