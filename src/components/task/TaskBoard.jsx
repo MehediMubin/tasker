6c092f32cb4272fa851e53dd522dace54b00cae6
MehediMubin <mehedi.cseulab201@gmail.com>
2024-10-18 19:36:23 +0600

    updated TaskBoard Component

--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -21,18 +21,16 @@ const TaskBoard = () => {
       setShowModal(!showModal);
    };
    return (
-      <>
+      <section className="mb-20" id="tasks">
          {showModal && <AddTaskModal />}
-         <section className="mb-20" id="tasks">
-            <div className="container">
-               <SearchTask />
-               <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-                  <TaskActions onAddTask={handleAddTask} />
-                  <TaskList tasks={tasks} />
-               </div>
+         <div className="container">
+            <SearchTask />
+            <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+               <TaskActions onAddTask={handleAddTask} />
+               <TaskList tasks={tasks} />
             </div>
-         </section>
-      </>
+         </div>
+      </section>
    );
 };
 
